fd82eb90e99d14e5c698fe6749af88fe695b32a9
Sofia.Kipina@urfu.me <Sofia.Kipina@urfu.me>
2024-09-23 13:34:09 +0500

    Задание 1

---
+++ 1task/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Буря");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Буря мгло небо кроет,");
+            Console.WriteLine("Вихри снежные крутя,");
+            Console.WriteLine("То как зверь она завоет,");
+            Console.WriteLine("То заплачет как дитя.");
+            Console.ReadKey();  
+        }
+    }
+}
