42c3cd6d8afdf5a3dbdf737975726facadeee7e4
Sofia.Kipina@urfu.me <Sofia.Kipina@urfu.me>
2024-09-30 13:27:36 +0500

    Задание 2

---
+++ task02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите внешний диаметр кольца:");
+            double diametr1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("введите внутренний диаметр кольца:");
+            double diametr2 = double.Parse(Console.ReadLine());
+
+            double S = (diametr1 - diametr2) * Math.PI;
+         
+            Console.WriteLine("S кольца=" + Math.Round(S, 3));
+            
+            Console.ReadKey();
+        }
+    }
+}
